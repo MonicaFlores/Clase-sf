f879717baa744597231cde594367a65b559eb06e
Monica Flores <mf3435@nyu.edu>
2018-11-06 13:11:51 -0300

    actualizacion operaciones con buffer

--- workshop-sf.R
+++ workshop-sf.R
@@ -15,7 +15,7 @@ shp_dir <- glue("{in_dir}/shapefiles")
 #RM zonas censales
 data_geo_RM <- glue("{shp_dir}/R13_Santiago") %>% 
   st_read()  %>%
-  # Sistema coordenadas UTM zone 19S
+  # Sistema coordenadas UTM zone 19S - metros
   st_transform(24879) %>% 
   rename_all(tolower) %>% 
   mutate(
@@ -43,9 +43,9 @@ metro_stgo <- glue("{shp_dir}/Estaciones_Metro_Santiago") %>%
 # Plotear mapa base
 mapa_base <- data_geo_RM %>% 
   ggplot() + 
-  geom_sf(color = "grey50") # +
+  geom_sf(color = "grey50") 
   #Sacarle coordenadas de ejes X, Y
-  #theme(axis.text.x = element_blank(), axis.text.y = element_blank())
+  # + theme(axis.text.x = element_blank(), axis.text.y = element_blank())
 mapa_base
 
 # Base + estaciones metro
@@ -85,18 +85,48 @@ mapa_base +
 
 # Dissolve buffer
 buf_tot <- st_union(buf)
-#Plotear buffer "disuelto"
+## Plotear buffer "disuelto"
 mapa_base +
-  geom_sf(data = buf_tot, color = "red")
+  geom_sf(data = buf_tot, color = "red", alpha = .5)
 
-#Seleccionar ZC dentro del buffer
-zc_buf_join <- st_join(data_geo_RM, buf_tot)
+# Seleccionar ZC dentro del buffers
 
-# subsetting
-zc_sel_buf <- as(zc_buf_join, "Spatial")
-
+#------------- Metodo 1: Intersectar
+## Intersectar
+buf_inters <- st_intersection(data_geo_RM, buf_tot) # 673 Observaciones
+## Plotear
 ggplot() +
-  geom_sf(data = zc_sel_buf)
+  geom_sf(data = buf_inters)
+# Plotear intersection arroja los atributos (ZC) cortados - como seleccionar el atributo completo?
+
+## Seleccionar solo geocodigo y hacerlo dataframe 
+## para que pierda atributos de geometría
+zc_buf <- as.data.frame(buf_inters) %>% select(geocode) 
+## Unirlo con el shapefile original ZC - inner join
+data_RM_buf <- data_geo_RM %>% inner_join(zc_buf, by = "geocode")
+## Plotear
+ggplot() +
+  geom_sf(data = data_RM_buf) +
+  ## Sobreponer buffer con transparencia
+  geom_sf(data = buf_tot, color = "red" , alpha = .5)
+# Funciona e incluye todas las ZC que intersectan el buffer
+
+#------------- Metodo 2: seleccionar solo las ZC cuyto centroide esta dentro del buffer
+## Covertir poligonos a puntos - centroide 
+zc_centr <- st_centroid(data_geo_RM)
+## Plotear sobre mapa base
+mapa_base + 
+  geom_sf(data = zc_centr, color = "grey40")
+## Intersectar con buffer
+buf_centr_inter <- st_intersection(zc_centr, buf_tot) # 463 Observaciones
+## Plotear interseccion
+mapa_base + 
+  geom_sf(data = buf_centr_inter, color = "navy") +
+  geom_sf(data = buf_tot, color = "red", alpha = .2)
+
+
+# Agregar datos a nivel espacial------------------------------------
+
 
 # Agregar datos escolaridad
   
@@ -107,8 +137,15 @@ ggplot() +
 # Comparar promedio ponderado dentro/fuera buffer metro
 # Tabla comparativa - esto es una operacion que involucra analisis espacial sin necesariamente visualizar un mapa 
   
-# Dot density map 
+# Dot density map - niveles escolaridad
+
   
 
+# Calcular area y controlar unidades -------------------------------
 
+area <- st_area(data_geo_RM) # Devuelve el valor en m2 siguiendo el sist de coordenadas
+area_ha <- area %>% units::set_units(ha)
+area_ha
+## Agregarlo como variable
+data_geo_RM$area_ha <- as.numeric(area_ha)
   
